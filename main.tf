c3e0ac16e6104681012437f17b00c3baae0100c4
guillADZ <guillaume.houriez@acknowledge.fr>
2023-09-05 11:46:58 +0200

    aa

--- main.tf
+++ main.tf
@@ -2,8 +2,3 @@ provider "azurerm" {
   features {}
   use_oidc = true
 }
-
-resource "azurerm_resource_group" "oidc" {
-  name     = var.resource_group_name
-  location = var.location
-}
--- terraform.tf
+++ terraform.tf
@@ -7,8 +7,16 @@ terraform {
   }
 
   backend "azurerm" {
-    key              = "terraform.tfstate"
+    key              = "IIS_Config.ps1"
     use_oidc         = true
     use_azuread_auth = true
+    resource_group_name = "env"
+    storage_account_name = "storageaccountsevranenv"
+    container_name = "data"
   }
+}
+
+resource "azurerm_resource_group" "app_grp" {
+  name = "rg-githubaction"
+  location = var.location
 }
