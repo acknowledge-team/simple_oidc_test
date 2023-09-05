7193dfffebae84e7e497f0bb7a6b7cf956e6bd6e
guillADZ <guillaume.houriez@acknowledge.fr>
2023-09-05 13:21:02 +0200

    upgrade

--- main.tf
+++ main.tf
@@ -1,4 +1,23 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name = "rg-shared"
+    storage_account_name = "stmhmtfcngd003"
+    container_name = "tfstate"
+    key = "terraform.state"
+  }
+  required_providers {
+    azurerm = {
+      version = "~>3.2"
+      source = "hashicorp/azurerm"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
-  use_oidc = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  location = "eastus"
+  name = "rg-test-github-actions"
 }
--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "location" {
-  description = "(Optional) Azure region to use. Defaults to East US."
-  default     = "eastus"
-}
